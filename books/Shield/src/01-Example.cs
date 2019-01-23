9c401eb443e9929ec87df7cbaf12588cd5f7133c
Pingvin8830 <pingvin8830@yandex.ru>
2019-01-23 19:55:56 +0300

    First C_Sharp pogramm

---
+++ books/Shield/src/01-Example.cs
@@ -0,0 +1,15 @@
+/*
+	Это простая C#-программа.
+
+	Назовём её Example.cs
+*/
+
+using System;
+
+class Example {
+	// Любая C#-программа начинается с вызова метода Main().
+	public static void Main() {
+		Console.WriteLine("Простая C#-программа.");
+	}
+}
+
